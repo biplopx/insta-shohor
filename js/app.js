36b880ab676dc688e15e00105e2b1806cf33b34a
MD Biplop Hossain <developerbiplop@gmail.com>
2022-03-10 22:40:00 +0600

    Liked and reported post showing double problem sloved

--- js/app.js
+++ js/app.js
@@ -143,6 +143,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  document.getElementById("liked").innerHTML = "<h1>Liked posts</h1>";
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
@@ -151,6 +152,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
+  document.getElementById("reported").innerHTML = "<h1>Reported posts</h1>";
   reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
